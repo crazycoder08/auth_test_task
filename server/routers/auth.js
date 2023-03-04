5147d785b5c316ae081180fa12ffdf2809350f7b
Crazy Coder <30525263+crazycoder08@users.noreply.github.com>
2023-03-04 15:26:38 +0530

    tete

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -5,6 +5,13 @@ const User = require("../models/User");
 const createHttpError = require("http-errors");
 const { signAccessToken, verifyAccessToken } = require("../helper/jwt_helper");
 
+const express = require("express");
+const router = express.Router();
+const { authSchema } = require("../helper/validation_schema");
+const User = require("../models/User");
+const createHttpError = require("http-errors");
+const { signAccessToken, verifyAccessToken } = require("../helper/jwt_helper");
+
 router.post("/register", async (req, res, next) => {
   try {
     const result = await authSchema.validateAsync(req.body);
@@ -13,8 +20,7 @@ router.post("/register", async (req, res, next) => {
       throw createHttpError.Conflict(`${result.email} is already exist!`);
 
     const user = new User(result);
-    const savedUser = await user.save();
-    await signAccessToken(savedUser.id);
+    await user.save();
 
     // res.send({ accessToken });
     res.send({ message: "User registered success" });
@@ -23,6 +29,55 @@ router.post("/register", async (req, res, next) => {
     next(err);
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const result = await authSchema.validateAsync(req.body);
+    const user = await User.findOne({ email: result.email });
+    if (!user) throw createHttpError.Unauthorized("User not registered");
+
+    const isMatch = await user.isValidPassword(result.password);
+    if (!isMatch)
+      throw createHttpError.Unauthorized("Username/password not valid");
+
+    const accessToken = await signAccessToken(user.id);
+    res.send({ accessToken });
+  } catch (error) {
+    if (error.isJoi === true) {
+      return next(createHttpError.BadRequest("Invalid username/password"));
+    }
+    next(error);
+  }
+});
+
+router.get("/user/:id", verifyAccessToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) throw createHttpError.Unauthorized("User not registered");
+
+    res.json({
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    });
+  } catch (error) {
+    res.send("error" + error);
+  }
+});
+
+router.get("/user", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (user) {
+      res.send(user);
+    }
+  } catch (error) {
+    res.send("error" + error);
+  }
+});
+
+module.exports = router;
+
 
 router.post("/login", async (req, res, next) => {
   try {
